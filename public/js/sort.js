20b1bcc274487483b61d45fb46f6d0e98f69fcd2
james-lepore <43451533+james-lepore@users.noreply.github.com>
2020-08-11 13:52:15 -0400

    Sort Working

--- public/js/sort.js
+++ public/js/sort.js
@@ -1,20 +1,41 @@
 $(document).ready(function() {
-    
+    function compare(option, b, i){
+        switch(option){
+            case "dl":
+                return b[i].getAttribute("dl") > b[i+1].getAttribute("dl");
+                break;
+            case "de":
+                return b[i].getAttribute("de") > b[i+1].getAttribute("de");
+                break;
+            case "al":
+                return b[i].innerText.toLowerCase() > b[i + 1].innerText.toLowerCase();
+                break;
+            case "r-al":
+                return b[i].innerText.toLowerCase() <= b[i + 1].innerText.toLowerCase();
+                break;
+            case "p":
+                return b[i].getAttribute("p") > b[i + 1].getAttribute("p");
+                break;
+            case "r-p":
+                return b[i].getAttribute("p") < b[i + 1].getAttribute("p");
+                break;
+            default:
+                return false;
+        }
+    }
     
     $('#sort').on('change', function() {
-        console.log("changed!");
+        let option = $(this).val();
+        console.log("changed to", option);
         let list, i, switching, b, shouldSwitch;
         list = document.getElementById("item_list");
-        console.log(list);
         switching = true;
         while(switching) {
             switching = false;
             b = list.getElementsByTagName("article");
-            console.log(b);
             for (i = 0; i < (b.length - 1); i++) {
                 shouldSwitch = false;
-                console.log(b[i].innerHTML);
-                if (b[i].innerText.toLowerCase() > b[i + 1].innerText.toLowerCase()) {
+                if (compare(option, b, i)) {
                   shouldSwitch = true;
                   break;
                 }
@@ -24,6 +45,6 @@ $(document).ready(function() {
                 switching = true;
             }
         }
-        // document.forms["sort-form"].submit();
+        console.log("done");
     });
 });
