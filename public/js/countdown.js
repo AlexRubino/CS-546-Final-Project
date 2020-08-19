9ea09d379e0d9ea0536fe08e09d4e4b5a440371f
Justin Wing <44987351+jwing25@users.noreply.github.com>
2020-08-18 21:11:26 -0400

    CSS for timer

--- public/js/countdown.js
+++ public/js/countdown.js
@@ -4,12 +4,8 @@ const countDownDate = new Date(endDate).getTime();
 
 // Update the count down every 1 second
 let x = setInterval(function() {
-
-// Get today's date and time
-let now = new Date().getTime();
-
 // Find the distance between now and the count down date
-let distance = countDownDate - now;
+let distance = countDownDate - new Date().getTime();
 
 // Time calculations for days, hours, minutes and seconds
 let days = Math.floor(distance / (1000 * 60 * 60 * 24));
@@ -17,13 +13,13 @@ let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
 let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
 let seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-// Display the result in the element with id="demo"
-document.getElementById("timer").innerHTML = days + "d " + hours + "h "
-+ minutes + "m " + seconds + "s ";
-
+// Display the result in the element with id="timer"
+document.getElementById("days").innerHTML = days;
+document.getElementById("hours").innerHTML =  hours;
+document.getElementById("minutes").innerHTML = minutes;
+document.getElementById("seconds").innerHTML = seconds;
 // If the count down is finished, write some text
 if (distance < 0) {
  clearInterval(x);
- document.getElementById("timer").innerHTML = "This item is now off the market!";
 }
 }, 1000);
