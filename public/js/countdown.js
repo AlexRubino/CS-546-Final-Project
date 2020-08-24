0f7112dd5fc07eb0950c693088dea83b130971ae
james-lepore <43451533+james-lepore@users.noreply.github.com>
2020-08-24 14:00:29 -0400

    timer error

--- public/js/countdown.js
+++ public/js/countdown.js
@@ -14,18 +14,24 @@ let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
 let seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
 // Display the result in the element with id="timer"
-document.getElementById("days").innerHTML = days;
-document.getElementById("hours").innerHTML =  hours;
-document.getElementById("minutes").innerHTML = minutes;
-document.getElementById("seconds").innerHTML = seconds;
+try {
+    document.getElementById("days").innerHTML = days;
+    document.getElementById("hours").innerHTML =  hours;
+    document.getElementById("minutes").innerHTML = minutes;
+    document.getElementById("seconds").innerHTML = seconds;
+} catch (e) {}
+
 // If the count down is finished, write some text
 if (distance < 0 ) {
     clearInterval(x);
-    document.getElementById("days").innerHTML = 0;
-    document.getElementById("hours").innerHTML =  0;
-    document.getElementById("minutes").innerHTML = 0;
-    document.getElementById("seconds").innerHTML = 0;
-    document.getElementById("new_bid_form").innerHTML = "Bidding is now over. This item has been sold";
+    try {
+        document.getElementById("days").innerHTML = 0;
+        document.getElementById("hours").innerHTML =  0;
+        document.getElementById("minutes").innerHTML = 0;
+        document.getElementById("seconds").innerHTML = 0;
+        document.getElementById("new_bid_form").innerHTML = "Bidding is now over. This item has been sold";
+    } catch (e) {}
+    
     
 // If the count down is finished, write some text
 }
